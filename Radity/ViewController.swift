f5adc08f90363c28c4e5dae206d149822788179b
Ilhad <ilhadmammadli@gmail.com>
2022-03-02 22:44:38 +0400

    Initial Commit

---
+++ Radity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Radity
+//
+//  Created by Ilhad Mammadli on 02.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
